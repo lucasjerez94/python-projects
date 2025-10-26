e0c72e04eaaf49400d416ed7682fd26d0ce9f10e
lucasjerez94 <lucasjerez9468@gmail.com>
2025-10-26 20:41:04 -0300

    agregando decoradores

---
+++ roadmap_backend/python_pro/decoradores.py
@@ -0,0 +1,28 @@
+"""
+¿Qué es un DECORADOR?
+Un decorador en Python es una función que modifica o extiende el comportamiento de otra función, sin cambiar su código original.
+Un decorador “envuelve” una función para agregarle algo antes o después de que se ejecute.
+"""
+#Ejemplo simple
+
+def saludar():
+    print('Hola Lucas')
+
+def decorador(funcion_original):
+    def nueva_funcion():
+        print('----Inicio----')
+        funcion_original()
+        print('----Final----')
+    return nueva_funcion
+
+
+saludo_mejorado = decorador(saludar)
+saludo_mejorado()
+
+
+#Forma "Pythonica" usando @
+@decorador
+def despedir():
+    print('Chau Lucas')
+
+despedir()
